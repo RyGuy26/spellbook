d542852a1f68c121a5155219ed4fa5569c168e1c
Ryan Manfred <rmanfred@purdue.edu>
2018-06-05 17:29:13 -0400

    adds 2nd form and list

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const addToSpells = function(ev) {
   const f = ev.target
   const spellName = f.spellName.value
   const spellsDiv = document.querySelector('#spells')
-  var points = document.createElement("ul");
+  var points = document.createElement("li");
   var node = document.createTextNode(spellName);
   points.appendChild(node);
   spellsDiv.appendChild(points);
+
+  const potionName = f.potionName.value
+  const potionDiv = document.querySelector('#potions')
+  var points = document.createElement("li");
+  var node = document.createTextNode(potionName);
+  points.appendChild(node);
+  potionDiv.appendChild(points);
+  f.reset()
+}
+const addToPotions = function(ev) {
+  const potionName = f.potionName.value
+  const potionDiv = document.querySelector('#potions')
+  var points = document.createElement("li");
+  var node = document.createTextNode(potionName);
+  points.appendChild(node);
+  potionDiv.appendChild(points);
   f.reset()
 }
 
+form.addEventListener('submit', addToPotions)
 form.addEventListener('submit', addToSpells)
