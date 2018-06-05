2f276c8913ce3fce63cbd91441e3ecad2b91b724
Ryan Manfred <rmanfred@purdue.edu>
2018-06-04 21:18:06 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('testing')
+const button = document.querySelector('button')
+const form = document.querySelector('input')
+const input = document.querySelector('input'.textContent)
+const sayContents = function() {
+ 
+  const heading = document.querySelector('#potions1')
+  heading.textContent = form.value
+}
+
+
+button.addEventListener('click', sayContents) 
+form.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    
+  
+    if (event.keyCode === 13) {
+      
+      button.click();
+    }
+  });
